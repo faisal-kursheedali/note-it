b2e352defcd6dff9c4a1b654c3471b4554c8ce36
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-07-12 18:01:36 +0530

    initiated react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    
+      <div className="bui-txt-head-xl">
+        hello world
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
